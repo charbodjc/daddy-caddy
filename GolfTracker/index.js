035b476cb09ee3ba7c76e55856975a624b5aa9ad
Dan Charbonneau <dan@hoobody.com>
2025-11-10 16:52:40 -0800

    fix: add URL polyfill for Hermes compatibility
    - Install react-native-url-polyfill package
    - Import polyfill at top of index.js before any other imports
    - Fixes 'Cannot assign to property protocol which has only a getter' error
    - Required for Expo + Hermes URL API compatibility
    - Resolves getManifestBaseUrl initialization crash

--- GolfTracker/index.js
+++ GolfTracker/index.js
@@ -2,6 +2,9 @@
  * @format
  */
 
+// URL polyfill for Hermes - must be imported first
+import 'react-native-url-polyfill/auto';
+
 import { AppRegistry } from 'react-native';
 import App from './App';
 
